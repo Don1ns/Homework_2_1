323fabeee66d6bb885592e0971fffe90b68d0716
Рияз <riaz.karimullin@yandex.ru>
2022-12-26 23:53:37 +0300

    ООП. Часть 1. Введение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CarBook carBook = new CarBook();
+        carBook.addCar("Lada", "Granta",-1.7f, "", 2015, "Россия");
+        carBook.addCar("Audi", "A8 50 L TDI quattro",3.0f, "черный", 2020, "Германия");
+        carBook.addCar("BMW", "Z8",3.0f, "черный", 2021, "Германия");
+        carBook.addCar("Kia", "Sportage 4",2.4f,"красный", 2018, "Южная Корея");
+        carBook.addCar("Hyundai", "Avante",1.6f, "оранжевый", 2016, "Южная Корея");
+
+        carBook.printAllCars();
     }
 }
