35300688e5c4462059a20755ad33957e847b6b91
Рияз <riaz.karimullin@yandex.ru>
2022-12-28 15:02:25 +0300

    ООП. Часть 1. Введение

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        CarBook carBook = new CarBook();
-        carBook.addCar("Lada", "Granta",-1.7f, "", 2015, "Россия");
-        carBook.addCar("Audi", "A8 50 L TDI quattro",3.0f, "черный", 2020, "Германия");
-        carBook.addCar("BMW", "Z8",3.0f, "черный", 2021, "Германия");
-        carBook.addCar("Kia", "Sportage 4",2.4f,"красный", 2018, "Южная Корея");
-        carBook.addCar("Hyundai", "Avante",1.6f, "оранжевый", 2016, "Южная Корея");
+        Car lada = new Car("Lada", "Granta",-1.7f, "", 2015, "Россия", "механическая", "седан", 346, 5, false);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro",3.0f, "черный", 2020, "Германия", "автоматическая", "седан", 768, 5, false);
+        Car bmw = new Car("BMW", "Z8",3.0f, "черный", 2021, "Германия", "механическая", "кабриолет", 656, 2, true);
+        Car kia = new Car("Kia", "Sportage 4",2.4f,"красный", 2018, "Южная Корея", "механическая", "внедорожник", 433, 5, false);
+        Car hyundai = new Car("Hyundai", "Avante",1.6f, "оранжевый", 2016, "Южная Корея", "автоматическая", "седан", 541, 5, true);
 
-        carBook.printAllCars();
     }
 }
